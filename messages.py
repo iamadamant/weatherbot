0b47a7ea28fa835e8f6de7c11b0704f88da02017
iamadamant <iam.adamant@mail.ru>
2020-07-26 15:16:01 +0300

    Comment was changed

--- messages.py
+++ messages.py
@@ -15,6 +15,9 @@ def get_full_weather_message(dt):
 	return msg
 
 def get_help_message():
+	"""Возвращяюет сообщение с краткой справкой
+	
+	"""
 	return 'Для получения сведений о погоде введите дату в формате \
 	%d.%m(Пример: 30.06) этот формат также можно изменить командой \
 	/setformat <format>(Например /setformat %d-%m)\n\n \
